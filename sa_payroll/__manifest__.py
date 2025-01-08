51549044aaa90db49bc2b8f92871a91c80e9e6c1
Pinakin Nayi <pinakin@erpweb.co.za>
2025-01-08 16:11:22 +0530

    SA: payroll

--- sa_payroll/__manifest__.py
+++ sa_payroll/__manifest__.py
@@ -52,6 +52,6 @@
     "price"       : 1000,
     "currency"    : "EUR",
     "images"      : ["static/description/banner.png",],
-    'license'     : 'OEEL-1',
+    'license'     : 'OPL-1',
 }
 
---
+++ sa_payroll/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.0.6 (trial), 000000, 2025-01-08T15:33:13.047181
+from .pyarmor_runtime import __pyarmor__
